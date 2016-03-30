8352486292c9ef483ae82720e99ddf09a8c72aeb
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-03-30 18:12:13 +0300

    Update urls.py

--- Django/urls.py
+++ Django/urls.py
@@ -4,5 +4,5 @@ from sitename.ajax import DataTablesAjax
 
 urlpatterns = patterns('',
     url(r'^admin/ajax/datatables/(?P<name>\w{0,50})/$',DataTablesAjax.as_view()),
-    url(r'^example', TemplateView.as_view(template_name="example.html", model='Model1'), name='example')
+    url(r'^example', TemplateView.as_view(template_name="example.html"), name='example')
 )
